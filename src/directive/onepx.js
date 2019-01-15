ad12b07fef076f946a2f7fe8a8dd38de8932b07e
HelloWorld20 <292791515@qq.com>
2019-01-15 14:10:41 +0800

    添加打包后资源

--- src/directive/onepx.js
+++ src/directive/onepx.js
@@ -1,4 +1,25 @@
 
+/**
+ * 解决1像素问题的vue指令
+ *
+ * 食用方法：
+ * 在给对应的DOM上加上对应指令即可
+ * v-onepx: 四边
+ * v-onepx-t: 顶边
+ * v-onepx-b: 底边
+ * v-onepx-l: 左边
+ * v-onepx-r: 右边
+ * data-border-color="#9d9d9d" 给边框加上自定义颜色
+ * data-border-outset="true" 使用boxshadow来实现边框，是往外发散的，有些特殊情况有用
+ * 传参：如果参数为falsy（除了undefined），则不渲染border，undefined默认渲染
+ *
+ * 自定义颜色：先读取标签属性data-border-color => 再读取css的border-color值 => 最后使用默认颜色
+ *
+ * 注意：
+ * 1、绑定的元素必须显示声明其position值为：relative、fixed、absolute之一
+ * 2、务必给对应的DOM清除掉border样式
+ * 3、不能用于<input type="text" />标签等内部不能插入元素的标签
+ */
 const dpr = devicePixelRatio;
 const DEFAULT_BORDER_COLOR = '#d9d9d9';
 export default {
