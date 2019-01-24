d439aec42aceae2aa247d6d8ddedffef963b1710
HelloWorld20 <292791515@qq.com>
2019-01-24 22:18:20 +0800

    fix demo again

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,73 @@
+<template>
+  <div id="app">
+    <div class="block">
+      <div>默认</div>
+      <div>指令</div>
+    </div>
+    <div class="block">
+      <div class="default radius" style="border-bottom: 1px solid">bottom</div>
+      <div class="demo radius" style="border-bottom: .5px solid">bottom</div>
+      <div class="demo radius" v-onepx-b>bottom</div>
+    </div>
+    <div class="block">
+      <div class="default radius" style="border-top: 1px solid">top</div>
+      <div class="demo radius harf" style="border-top: .5px solid">top</div>
+      <div class="demo radius" v-onepx-t>top</div>
+    </div>
+    <div class="block">
+      <div class="default radius" style="border-left: 1px solid">left</div>
+      <div class="demo radius harf" style="border-left: .5px solid">left</div>
+      <div class="demo radius" v-onepx-l>left</div>
+    </div>
+    <div class="block">
+      <div class="default radius" style="border-right: 1px solid">right</div>
+      <div class="demo radius harf" style="border-right: .5px solid">right</div>
+      <div class="demo radius" v-onepx-r>right</div>
+    </div>
+    <div class="block">
+      <div class="default radius margin">margin</div>
+      <div class="default radius harf margin">margin</div>
+      <div class="demo radius margin" v-onepx>margin</div>
+    </div>
+    <div class="block">
+      <div class="default radius padding">padding</div>
+      <div class="default radius harf padding">padding</div>
+      <div class="demo radius padding" v-onepx>padding</div>
+    </div>
+    <div class="block">
+      <div class="default radius border-box">border-box</div>
+      <div class="default radius harf border-box">border-box</div>
+      <div class="demo radius border-box" v-onepx>border-box</div>
+    </div>
+    <div class="block">
+      <div class="default radius content-box">content-box</div>
+      <div class="default radius harf content-box">content-box</div>
+      <div class="demo radius content-box" v-onepx>content-box</div>
+    </div>
+    <div class="block">
+      <div class="default radius translate">translate</div>
+      <div class="default radius harf translate">translate</div>
+      <div class="demo radius translate" v-onepx>translate</div>
+    </div>
+    <div class="block">
+      <div class="default radius scale">scale</div>
+      <div class="default radius harf scale">scale</div>
+      <div class="demo radius scale" v-onepx>scale</div>
+    </div>
+    <div class="block">
+      <div class="default radius rotate">rotate</div>
+      <div class="default radius harf rotate">rotate</div>
+      <div class="demo radius rotate" v-onepx>rotate</div>
+    </div>
+    <div class="block">
+      <input type="text" placeholder="demo" class="default input">
+      <input type="text" placeholder="demo" class="default  harf input">
+      <input type="text" placeholder="demo" v-onepx class="demo input">
+    </div>
+  </div>
+</template>
+
+
 <script>
 let blockElementConfig = [
   {
@@ -60,130 +130,73 @@ let blockElementConfig = [
 ];
 export default {
   name: "app",
-  render(h) {
-    return h('div', {
-        domProps: {
-          id: 'app'
-        },
-      },
-      blockElementConfig.map(v => {
-        return h('div', {
-            domProps: {
-              className: 'block'
-            }
-          },
-          [
-            v.tag === 'input' ?
-            h('input', {
-              domProps: {
-                className: `default radius ${v.name}`,
-                value: v.name
-              }
-            }) :
-            h('div', {
-              domProps: {
-                className: v.title ? '' : `default radius ${v.name}`,
-                innerText: v.title ? '原生1px' : v.name
-              }
-            })
-            ,
-            v.tag === 'input' ?
-            h('input', {
-              domProps: {
-                className: `device radius ${v.name}`,
-                value: v.name
-              }
-            }) :
-            h('div', {
-              domProps: {
-                className: v.title ? '' : `device radius harf ${v.name}`,
-                innerText: v.title ? '原生0.5px' : v.name
-              }
-            })
-            ,
-            v.tag === 'input' ?
-            h('input', {
-              domProps: {
-                className: `demo radius ${v.name}`,
-                value: v.name
-              },
-              directives: [{name: v.directivesName}]
-            }) :
-            h('div', {
-              domProps: {
-                className: v.title ? '' : `demo radius`,
-                innerText: v.title ? '指令' : v.name
-              },
-              directives: [{name: v.directivesName}]
-            })
-          ]
-        )
-      })
-    )
-  },
+  // render(h) {
+  //   return h('div', {
+  //       domProps: {
+  //         id: 'app'
+  //       },
+  //     },
+  //     blockElementConfig.map(v => {
+  //       return h('div', {
+  //           domProps: {
+  //             className: 'block'
+  //           }
+  //         },
+  //         [
+  //           v.tag === 'input' ?
+  //           h('input', {
+  //             domProps: {
+  //               className: `default radius ${v.name}`,
+  //               value: v.name
+  //             }
+  //           }) :
+  //           h('div', {
+  //             domProps: {
+  //               className: v.title ? '' : `default radius ${v.name}`,
+  //               innerText: v.title ? '原生1px' : v.name
+  //             }
+  //           })
+  //           ,
+  //           v.tag === 'input' ?
+  //           h('input', {
+  //             domProps: {
+  //               className: `device radius ${v.name}`,
+  //               value: v.name
+  //             }
+  //           }) :
+  //           h('div', {
+  //             domProps: {
+  //               className: v.title ? '' : `device radius harf ${v.name}`,
+  //               innerText: v.title ? '原生0.5px' : v.name
+  //             }
+  //           })
+  //           ,
+  //           v.tag === 'input' ?
+  //           h('input', {
+  //             domProps: {
+  //               className: `demo radius ${v.name}`,
+  //               value: v.name
+  //             },
+  //             directives: [{name: v.directivesName}]
+  //           }) :
+  //           h('div', {
+  //             domProps: {
+  //               className: v.title ? '' : `demo radius ${v.name}`,
+  //               innerText: v.title ? '指令' : v.name
+  //             },
+  //             directives: [{name: v.directivesName}]
+  //           })
+  //         ]
+  //       )
+  //     })
+  //   )
+  // },
   mounted() {
     this.$el.querySelector("input").innerHTML = "<p>some text you can't see</p>";
   }
 };
 // render对应的dom结构如下：
-// <div id="app">
-//     <div class="block">
-//       <div>默认</div>
-//       <div>指令</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius bottom">bottom</div>
-//       <div class="demo radius bottom" v-onepx-b>bottom</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius top" style>top</div>
-//       <div class="demo radius top" v-onepx-t>top</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius left">left</div>
-//       <div class="demo radius left" v-onepx-l>left</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius right">right</div>
-//       <div class="demo radius right" v-onepx-r>right</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius margin">margin</div>
-//       <div class="demo radius margin" v-onepx>margin</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius padding">padding</div>
-//       <div class="demo radius padding" v-onepx>padding</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius border-box">border-box</div>
-//       <div class="demo radius border-box" v-onepx>border-box</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius content-box">content-box</div>
-//       <div class="demo radius content-box" v-onepx>content-box</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius translate">translate</div>
-//       <div class="demo radius translate" v-onepx>translate</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius scale">scale</div>
-//       <div class="demo radius scale" v-onepx>scale</div>
-//     </div>
-//     <div class="block">
-//       <div class="default radius rotate">rotate</div>
-//       <div class="demo radius rotate" v-onepx>rotate</div>
-//     </div>
-//     <div>
-//       <input type="text" placeholder="demo" class="default input">
-//       <input type="text" placeholder="demo" v-onepx class="demo input">
-//     </div>
-//     <div class="block">
-//       <div class="default radius rotate">rotate</div>
-//       <div class="demo radius rotate" v-onepx>rotate</div>
-//     </div>
-//   </div>
+
 </script>
 
 <style>
@@ -232,19 +245,19 @@ div.harf{
 }
 .bottom{
   border: none;
-  border-bottom: 1px solid;
+  /* border-bottom: 1px solid; */
 }
 .top{
   border: none;
-  border-top: 1px solid;
+  /* border-top: 1px solid; */
 }
 .left{
   border: none;
-  border-left: 1px solid;
+  /* border-left: 1px solid; */
 }
 .right{
   border: none;
-  border-right: 1px solid;
+  /* border-right: 1px solid; */
 }
 .margin {
   margin-left: 30px;
